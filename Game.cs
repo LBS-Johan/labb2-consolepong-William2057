a98140bab4ec47e33b761751d149f6621cfaa2bc
William2057 <Williamarnell882@gmail.com>
2025-10-16 14:38:00 +0200

    Removed Empty Spaces

--- Game.cs
+++ Game.cs
@@ -12,25 +12,19 @@ namespace Labb2_ConsolePong
         int width;
         int height;
         Paddle rightPlayer = new Paddle(20, 10, 3);
-        
-
-
         public void StartGame()
         {
             // Setup konsol-fönstret
             width = Console.WindowWidth;
             height = Console.WindowHeight;
             Console.CursorVisible = false;
-            
         }
-
         public bool Run()
         {
             Console.Clear();
             //Töm hela skärmen i början av varje uppdatering.
             
             rightPlayer.Draw();
-
             if (Input.IsPressed(ConsoleKey.UpArrow))
             {
                 rightPlayer.Move(1);
@@ -50,7 +44,6 @@ namespace Labb2_ConsolePong
             {
                 //Flytta spelare 2 nedåt
             }
-
 
 
             //Return true om spelet ska fortsätta
