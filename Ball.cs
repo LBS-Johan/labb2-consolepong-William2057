73f3345a247c33d57260b74e6afb8d3565c642bc
William2057 <Williamarnell882@gmail.com>
2025-10-21 09:23:09 +0200

    Created LeftPlayer & Made GameBall Move

---
+++ Ball.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_ConsolePong
+{
+    class Ball
+    {
+        int x;
+        int y;
+        int xVelocity;
+        int yVelocity;
+
+        public Ball(int x, int y, int xVelocity, int yVelocity)
+        {  //Fråga Johan om vilken position bollen ska vara på skärmen
+            this.x = x;
+            this.y = y;
+            this.xVelocity = xVelocity;
+            this.yVelocity = yVelocity;
+        }
+        public void Move()
+        {
+            x += xVelocity;
+            y += yVelocity;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("o");
+        }
+        void CheckCollisions(Paddle leftPlayer, Paddle rightPlayer, int width, int height)
+        {
+
+        }
+
+    }
+}
--- Game.cs
+++ Game.cs
@@ -11,7 +11,9 @@ namespace Labb2_ConsolePong
     {
         int width;
         int height;
+        Paddle LeftPlayer = new Paddle(5, 10, 3);
         Paddle rightPlayer = new Paddle(115, 10, 3);
+        Ball gameBall = new Ball(20, 10, 1, 1);
         public void StartGame()
         {
             // Setup konsol-fönstret
@@ -25,6 +27,10 @@ namespace Labb2_ConsolePong
             //Töm hela skärmen i början av varje uppdatering.
             
             rightPlayer.Draw();
+            LeftPlayer.Draw();
+            gameBall.Move();
+            gameBall.Draw();
+            
             if (Input.IsPressed(ConsoleKey.UpArrow))
             {
                 rightPlayer.Move(1);
@@ -39,10 +45,12 @@ namespace Labb2_ConsolePong
             if (Input.IsPressed(ConsoleKey.W))
             {
                 //Flytta spelare 2 uppåt
+                LeftPlayer.Move(1);
             }
             if (Input.IsPressed(ConsoleKey.S))
             {
                 //Flytta spelare 2 nedåt
+                LeftPlayer.Move(-1);
             }
 
 
